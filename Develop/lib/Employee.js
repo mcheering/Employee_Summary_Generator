f5570594f0de15e6a484da0d682025c114259b64
mcheering <62622009+mcheering@users.noreply.github.com>
2021-01-23 16:27:18 -0600

    uploading project. Needs some formatting on the cards.  Will add margin to them.  Also, will adjust colors.  Want to refactor code some to make it more succinct.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,27 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+      constructor(name, email, id) {
+            this.name = name
+            this.email = email
+            this.id = id
+      }
+      getName() {
+            return this.name;
+      }
+
+      getId() {
+            return this.id;
+      }
+
+      getEmail() {
+            return this.email;
+      }
+      getRole() {
+            return "Employee";
+      }
+}
+
+
+
+
+module.exports = Employee
